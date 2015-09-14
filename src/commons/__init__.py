033e9121c4cb9fd3f05e6913ec7ef24127c76d72
joamag <joamag@gmail.com>
2015-09-14 16:37:11 +0100

    new commons python support

--- src/commons/__init__.py
+++ src/commons/__init__.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from . import controllers
-from . import models
-from . import main
+from . import structures
 
-from .controllers import *
-from .models import *
-from .main import BudyApp
+from .structures import Decimal
