68489dedf7314d74dbb36f4eae93973652370da2
joamag <joamag@gmail.com>
2015-10-31 00:50:19 +0000

    new round support in commons

--- src/commons/__init__.py
+++ src/commons/__init__.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from . import dict
+from . import round
 from . import structures
 
 from .dict import dict_merge
+from .round import roundi, rounds, roundt, round_apply, round_unapply, round_is_new
 from .structures import Decimal
